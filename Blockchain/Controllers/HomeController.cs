4f4f2c540ba67f31f848c098482b9a0824545a7f
Shane White <shane.p.white@gmail.com>
2018-03-22 20:22:57 -0500

    Add project files.

---
+++ Blockchain/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blockchain.Models;
+
+namespace Blockchain.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var client = new RestClient("https://api.coinmarketcap.com/v1/ticker/");
+            var request = new RestRequest(Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+
+            IEnumerable<Coins> coincap = JsonConvert.DeserializeObject<IEnumerable<Coins>>(response.Content);
+            return View(coincap);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
